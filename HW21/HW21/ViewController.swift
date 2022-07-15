c60a076f55a2e35951b9830d99ff49293186ee3c
Makulov Evgeny <makulovev@gmail.com>
2022-07-16 00:09:19 +0300

    Implemented logic and sync

--- HW21/HW21/ViewController.swift
+++ HW21/HW21/ViewController.swift
@@ -8,54 +8,92 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Set UI elements
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var againButton: UIButton!
+    @IBOutlet weak var again: UIButton!
+    @IBOutlet weak var create: UIButton!
     
-    var isBlack: Bool = false {
-        didSet {
-            if isBlack {
-                self.view.backgroundColor = .black
-            } else {
-                self.view.backgroundColor = .white
+    
+    @IBAction func searchButton(_ sender: UIButton) {
+        self.bruteForce(passwordToUnlock: textField.text ?? "")
+        self.create.isHidden = true
+        label.isHidden = false
+
+    }
+    
+    @IBAction func ganerateButton(_ sender: UIButton) {
+        textField.text = "\(randomPasswordGenerate())"
+        self.button.isHidden = false
+        self.create.isHidden = true
+
+    }
+    @IBAction func againButton(_ sender: UIButton) {
+        textField.text = ""
+        label.text = ""
+        textField.isSecureTextEntry = true
+        create.isHidden = false
+        again.isHidden = true
+
+    }
+    
+    
+    //MARK: - Lifecycle
+override func viewDidLoad() {
+    super.viewDidLoad()
+    activityIndicator.isHidden = true
+    textField.isSecureTextEntry = true
+    again.isHidden = true
+    button.isHidden = true
+    label.isHidden = true
+
+
+}
+    // MARK: - Functions
+func bruteForce(passwordToUnlock: String) {
+    DispatchQueue.main.async {
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+        self.button.isHidden = true
+
+
+        
+        DispatchQueue.global(qos: .background).async {
+            let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
+            var password: String = ""
+            while password != passwordToUnlock {
+                password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+                
+                DispatchQueue.main.async {
+                    self.label.text = "Hacking.. \(password)"
+                    if password == passwordToUnlock {
+                        self.activityIndicator.stopAnimating()
+                        self.activityIndicator.isHidden = true
+                        self.textField.isSecureTextEntry = false
+                        self.label.text = "Hacked! Password: \(password)"
+                        self.again.isHidden = false
+                        self.button.isHidden = true
+
+                    }
+                }
             }
         }
     }
-    
-    @IBAction func onBut(_ sender: Any) {
-        isBlack.toggle()
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-//        self.bruteForce(passwordToUnlock: "1!gr")
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    func bruteForce(passwordToUnlock: String) {
-        let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
-
-        var password: String = ""
-
-        // Will strangely ends at 0000 instead of ~~~
-        while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
-            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
-//             Your stuff here
-            print(password)
-            // Your stuff here
-        }
-        
-        print(password)
-    }
-    @IBAction func pressButton(_ sender: Any) {
-    }
 }
-
-
-
+    
+    func randomPasswordGenerate() -> String {
+        let passwordLength = 3
+        let passwordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        let randomPassword = String((0..<passwordLength).compactMap{ _ in passwordCharacters.randomElement() })
+        return randomPassword
+    }
+    
+}
+    // MARK: - Extensions
 extension String {
     var digits:      String { return "0123456789" }
     var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
@@ -72,7 +110,7 @@ extension String {
         self = String(stringArray)
     }
 }
-
+    // MARK: - Brut force
 func indexOf(character: Character, _ array: [String]) -> Int {
     return array.firstIndex(of: String(character))!
 }
